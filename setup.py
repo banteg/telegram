f6bf4267efdff01f7de7826965d6dacf143cc73e
banteg <banteeg@gmail.com>
2016-09-12 04:14:43 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='telegram',
+    version='1.0',
+    description='A minimal Telegram API wrappper',
+    url='https://github.com/banteg/telegram',
+    py_modules=['telegram'],
+    install_requires=['requests'],
+)
