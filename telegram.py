028b16a2882fc416e350b06751c6755d848bb053
banteg <banteeg@gmail.com>
2016-09-12 04:14:29 +0700

    add minimal telegram api wrapper

---
+++ telegram.py
@@ -0,0 +1,20 @@
+import requests
+from functools import partial
+
+
+class TelegramApi:
+
+    def __init__(self, token):
+        self.url = 'https://api.telegram.org/bot{token}/'.format_map(locals())
+
+    def __getattr__(self, name):
+        method = self.snake_to_camel(name)
+        return partial(self.call, method)
+
+    def call(self, method, **params):
+        response = requests.get(self.url + method, params=params)
+        return response.json()
+
+    def snake_to_camel(self, text):
+        words = text.split('_')
+        return words[0] + ''.join([w.title() for w in words[1:]])
