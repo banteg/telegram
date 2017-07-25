130cf2d6706b13945beb22149fbf2e417e74fe92
banteg <banteeg@gmail.com>
2017-07-26 00:39:10 +0700

    bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tgb',
-    version='1.0.1',
+    version='1.0.2',
     description='A minimal Telegram API wrappper',
     url='https://github.com/banteg/telegram',
     py_modules=['telegram'],
